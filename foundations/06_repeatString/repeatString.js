01c427560aa4923940715e6603e2c8e2a21c9486
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-01-29 05:22:33 -0800

    had a blank result variable to store the result. and keep on repeating the word until it reaches the amount of times.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    result = ""
+    if (b < 0) {
+        return "ERROR"
+    }
+    for (i = 0; i < b; i++) {
+        result += a
+    }
+    return result
 };
 
 // Do not edit below this line
