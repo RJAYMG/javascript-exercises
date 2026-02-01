1c494899f71da3e4d7b398e08dcd7c28f6fec9fb
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-02-01 02:56:33 -0800

    i just converted the formula into the function

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  const convertCelsius = (number - 32)*(5/9)
+  const round = parseFloat(convertCelsius.toFixed(1))
+  return round
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  const convertFahrenheit = (number)*(9/5)+32
+  const round = parseFloat(convertFahrenheit.toFixed(1))
+  return round
 };
 
+
+console.log(convertToCelsius(32))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
