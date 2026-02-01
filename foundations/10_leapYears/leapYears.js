82a7df32b675b8fe57bdcd4d9d8cb93464e131ee
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-02-01 02:26:45 -0800

    i just added the conditionals

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    if (number % 400 === 0) {return true}
+    if (number % 100 === 0) {return false}
+    if (number % 4 === 0) {return true}
+    else {return false}
 };
 
+console.log(leapYears(1900))
 // Do not edit below this line
 module.exports = leapYears;
