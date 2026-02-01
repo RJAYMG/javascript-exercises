fb576f9b20188d2e738163f1a38cd481286eeb98
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-01-31 18:06:02 -0800

    the first build was only centered on a starting and end value so it would know when to stop the loop it also had a step so it would keep counting till it reached the end value.
    now it was just conditionals to catch non numbers
    and negative numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end, step=1) {
+    const result = [];
+    if (start < 0 || end < 0) {
+        return "ERROR"
+    }
 
+    if (!(Number.isInteger(start)) || !(Number.isInteger(end))) {
+        return "ERROR"
+    }
+
+    if (start < end) {
+        for (i = start; i <= end; i += step) {
+        result.push(i);
+        }
+    } else {
+        for (i = end; i <= start; i += step) {
+        result.push(i);
+        }
+    }
+
+    const mappedRes = result.reduce((item, current) => {
+        return item + current;
+    })
+    return mappedRes
 };
+
+console.log(sumAll(2.5, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
