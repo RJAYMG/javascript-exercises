accadb2cfbbb131043daaeabb50230255c3c0a77
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-06-24 19:20:35 +0800

    i forgot how to reverse an array... yeah that was rough. but i figured it out and also i don't understand regex. :^

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const cleanWord =  word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reverseWord = cleanWord.split("").reverse().join("")
+    
+    return cleanWord === reverseWord
 };
 
 // Do not edit below this line
