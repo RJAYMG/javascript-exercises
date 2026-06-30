883e8313a8fd23f8be2589f03bb23e739f3b14a2
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-06-30 18:38:32 +0800

    commit didn't work... i learned how to write a function for the fibonacci sequence. and also kinda how to work with objects like checking for properties inside a object

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
+    let next;
 
+    const parsedNum = Number(num)
+    if (parsedNum === 0) {
+        return 0;
+    }
+    else if (parsedNum < 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i<=parsedNum; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        let currentDeathYear;
+        let oldestDeathYear;
+        if (!Object.hasOwn(currentPerson, "yearOfDeath")) {
+            currentDeathYear = new Date().getFullYear()
+        } else {
+            currentDeathYear = currentPerson.yearOfDeath
+        }
+        if (!Object.hasOwn(oldestPerson, "yearOfDeath")) {
+            oldestDeathYear = new Date().getFullYear()
+        } else {
+            oldestDeathYear = oldestPerson.yearOfDeath
+        }
+        const oldestPersonAge = oldestDeathYear - oldestPerson.yearOfBirth
+        const currentPersonAge = currentDeathYear - currentPerson.yearOfBirth
+        if (currentPersonAge > oldestPersonAge) {
+            return currentPerson
+        } else {
+            return oldestPerson
+        }
+    })
 };
 
 // Do not edit below this line
